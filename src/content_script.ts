d7076e3ba203278cef690c31b0cd2dc9846275e2
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-02-26 03:42:24 +0900

    自分が”いいね”を付けたときだけハイライト表示する

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,23 +1,39 @@
+const visibleLikeToClicked = () => {
+    const likeBtnEls = document.getElementsByClassName("ocean-ui-comments-commentbase-like-count") as HTMLCollectionOf<HTMLElement>;
+    const likeEls = document.getElementsByClassName("ocean-ui-comments-commentbase-like") as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < likeEls.length; i++) {
+        const likeEl = likeEls.item(i) as HTMLElement;
+        if (likeEl.textContent === 'いいね！を取り消す') {
+            const likeBtnEl = likeBtnEls.item(i) as HTMLElement;
+            likeBtnEl.classList.add('ocean-ui-comments-commentbase-like-count-active');
+        }
+    }
+};
+
 const hideLikeConts = () => {
     const likeCountEls = document.getElementsByClassName("ocean-ui-comments-commentbase-like-count-text") as HTMLCollectionOf<HTMLElement>;
+    const likeBtnEls = document.getElementsByClassName("ocean-ui-comments-commentbase-like-count") as HTMLCollectionOf<HTMLElement>;
+
     for (const el of likeCountEls) {
-        el.style.display = 'none'
+        el.style.display = 'none';
     }
 
-    const likeBtnEls = document.getElementsByClassName("ocean-ui-comments-commentbase-like-count") as HTMLCollectionOf<HTMLElement>;
     for (const el of likeBtnEls) {
         el.classList.remove('ocean-ui-comments-commentbase-like-count-active');
     }
-}
+};
 
 const addObserverIfDesiredNodeAvailable = () => {
-    const commentComponentEl = document.querySelector('.ocean-ui-comments-commentcomponent');
-    if (!commentComponentEl) {
-        window.setTimeout(addObserverIfDesiredNodeAvailable, 300);
+    const contentBody = document.getElementById('contents-body-ocean');
+    if (!contentBody) {
         return;
     }
     hideLikeConts();
-    const observer = new MutationObserver(hideLikeConts);
-    observer.observe(commentComponentEl, {childList: true});
-}
+    visibleLikeToClicked();
+    const hideLikeContsObserver = new MutationObserver(hideLikeConts);
+    hideLikeContsObserver.observe(contentBody, {childList: true, subtree: true});
+    const visibleLiketoClickedObserver = new MutationObserver(visibleLikeToClicked);
+    visibleLiketoClickedObserver.observe(contentBody, {childList: true, subtree: true});
+};
 addObserverIfDesiredNodeAvailable();

ff627571370804644e7983ca8922055344f23d0a
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-02-26 01:21:20 +0900

    Chrome拡張を作っていくための準備

---
+++ webpack/webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = '../src/';
+
+module.exports = {
+    entry: {
+        popup: path.join(__dirname, srcDir + 'popup.ts'),
+        background: path.join(__dirname, srcDir + 'background.ts'),
+        content_script: path.join(__dirname, srcDir + 'content_script.ts')
+    },
+    output: {
+        path: path.join(__dirname, '../dist/js'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    plugins: [
+        // exclude locale files in moment
+        new CopyPlugin([
+            { from: '.', to: '../' }
+          ],
+          {context: 'public' }
+        ),
+    ]
+};

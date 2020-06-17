10bbf5f4e088adb7f939b4809849aeb2c82a2dc4
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-06-17 11:15:08 +0900

    webpack.jsに統合する

---
+++ webpack.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = 'src/';
+
+module.exports = {
+    entry: {
+        content_script: path.join(__dirname, srcDir + 'ContentScript.ts'),
+    },
+    output: {
+        path: path.join(__dirname, 'dist/js'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    plugins: [new CopyPlugin([{ from: '.', to: '../' }], { context: 'public' })],
+};
